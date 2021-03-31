fc19dd230d6bd712cf5dcbe4da51e72924fb5b63
orlandojtx <46915982+orlandojtx@users.noreply.github.com>
2021-03-31 14:04:16 -0500

    square
    perimeter,surface

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Suma k = new Suma();
+        double n1;
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            n1 = double.Parse(textBox1.Text);
+
+            textBox3.Text = k.Per(n1).ToString();
+            textBox2.Text = k.Sur(n1).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            textBox2.Text = null;
+            textBox3.Text = null;
+        }
+    }
+}
---
+++ suma.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Suma
+    {
+        public double Per(double n1)
+        {
+            return n1*4;
+        }
+        public double Sur(double n1)
+        {
+            return n1 * n1;
+        }
+    }
+}
